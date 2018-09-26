2e2e30c2c2d0b48e2fdea6dc2c2968e63a17c4e8
Tom Goetz <tcgoetz@gmail.com>
2018-09-26 18:24:02 -0400

    fill in a bunch of GC enums

--- GarminConnectEnums.py
+++ GarminConnectEnums.py
@@ -14,12 +14,12 @@ logger = logging.getLogger(__file__)
 class Event(enum.Enum):
     race            = 1
     recreation      = 2
-    fitness         = 8
+    special_event   = 3
+    training        = 4
+    transportation  = 5
+    touring         = 6
     geocaching      = 7
-    # special_event
-    # touring
-    # training
-    # transportation
+    fitness         = 8
     uncategorized   = 9
 
     @classmethod
@@ -43,23 +43,56 @@ class Sport(enum.Enum):
     track_running                   = 8
     walking                         = 9
     road_biking                     = 10
+    indoor_cardio                   = 11
     strength_training               = 13
     casual_walking                  = 15
+    speed_walking                   = 16
     top_level                       = 17
     treadmill_running               = 18
+    cyclocross                      = 19
+    downhill_biking                 = 20
+    track_cycling                   = 21
+    recumbent_cycling               = 22
+    indoor_cycling                  = 25
     swimming                        = 26
+    lap_swimming                    = 27
+    open_water_swimming             = 28
     fitness_equipment               = 29
     elliptical                      = 30
     stair_climbing                  = 31
+    indoor_rowing                   = 32
     snow_shoe                       = 36
+    mountaineering                  = 37
     rowing                          = 39
+    wind_kite_surfing               = 41
+    horseback_riding                = 44
+    driving_general                 = 49
+    flying                          = 52
     paddling                        = 57
+    whitewater_rafting_kayaking     = 60
     skating                         = 62
     inline_skating                  = 63
     resort_skiing_snowboarding      = 67
+    backcountry_skiing_snowboarding = 68
+    boating                         = 75
+    sailing                         = 77
     cross_country_skiing            = 81
     stand_up_paddleboarding         = 87
+    golf                            = 88
+    bmx                             = 131
+    hunting_fishing                 = 133
+    surfing                         = 137
+    wakeboarding                    = 138
+    rock_climbing                   = 139
+    hang_gliding                    = 140
+    tennis                          = 142
+    gravel_cycling                  = 143
     diving                          = 144
+    yoga                            = 149
+    floor_climbing                  = 150
+    virtual_ride                    = 152
+    virtual_run                     = 153
+    obstacle_run                    = 154
     indoor_running                  = 156
 
     @classmethod
