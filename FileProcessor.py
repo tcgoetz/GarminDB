c54c5ce6061c2792522825f778b305a2434b5682
Tom Goetz <tcgoetz@gmail.com>
2019-04-20 08:41:10 -0400

    FileProcessor refactor

--- FileProcessor.py
+++ FileProcessor.py
@@ -13,12 +13,19 @@ logger = logging.getLogger(__file__)
 class FileProcessor():
 
     @classmethod
+    def regex_matches_file(cls, file, file_regex):
+        return re.search(file_regex, file)
+
+    @classmethod
     def match_file(cls, input_file, file_regex):
         logger.info("Reading file: " + input_file)
-        match = re.search(file_regex, input_file)
-        if match:
+        if cls.regex_matches_file(input_file, file_regex):
             return [input_file]
         return []
+
+    @classmethod
+    def file_newer_than(cls, file, timestamp):
+        return datetime.datetime.fromtimestamp(os.stat(file).st_ctime) > timestamp
 
     @classmethod
     def dir_to_files(cls, input_dir, file_regex, latest=False):
@@ -26,8 +33,7 @@ class FileProcessor():
         file_names = []
         timestamp = datetime.datetime.now() - datetime.timedelta(1)
         for file in os.listdir(input_dir):
-            match = re.search(file_regex, file)
             file_with_path = input_dir + "/" + file
-            if match and (not latest or datetime.datetime.fromtimestamp(os.stat(file_with_path).st_ctime) > timestamp):
+            if cls.regex_matches_file(file, file_regex) and (not latest or cls.file_newer_than(file_with_path, timestamp)):
                 file_names.append(file_with_path)
         return file_names
