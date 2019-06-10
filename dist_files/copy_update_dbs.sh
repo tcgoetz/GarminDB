ba86fe1e108b3c759b757ce3946402db7d51f361
Tom Goetz <tcgoetz@gmail.com>
2019-06-10 19:59:59 -0400

    fix copy script

--- dist_files/copy_update_dbs.sh
+++ dist_files/copy_update_dbs.sh
@@ -3,5 +3,5 @@
 # Use this script to update existing DBs by copying daily monitoring and activity files from a USB connected Garmin device.
 echo "Updating existing DBs by copying from a USB mounted Garmin device"
 
-./garmin --all --download --import --analyze --latest
+./garmin --all --copy --import --analyze --latest
 
