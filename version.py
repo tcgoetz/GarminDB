d774c40b2bfe01a55f5582d42a24aad4954d8383
Tom Goetz <tcgoetz@gmail.com>
2019-07-26 08:08:16 -0400

    v1.0.4

--- version.py
+++ version.py
@@ -4,5 +4,5 @@ __author__ = "Tom Goetz"
 __copyright__ = "Copyright Tom Goetz"
 __license__ = "GPL"
 
-version_info = (1, 0, 3)
+version_info = (1, 0, 4)
 version = '.'.join(str(digit) for digit in version_info)
