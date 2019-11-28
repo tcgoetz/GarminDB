24023e1789d8e8de9c108023b95aeb10e67f9776
Tom Goetz <tcgoetz@gmail.com>
2019-11-28 09:07:52 -0500

    updates to the bug report script: python3, package list, dividers

--- bugreport.sh
+++ bugreport.sh
@@ -2,8 +2,8 @@
 
 EPOCH=`date +'%s'`
 BUGREPORT=bugreport.txt
-PYTHON=python
-PYTHON_PACKAGES="sqlalchemy requests python-dateutil enum34 progressbar2 PyInstaller matplotlib"
+PYTHON=python3
+PYTHON_PACKAGES="sqlalchemy requests python-dateutil progressbar2 PyInstaller matplotlib"
 
 zip -j -r bugreport_${EPOCH}.zip *.log
 
@@ -11,12 +11,15 @@ uname -a > ${BUGREPORT}
 which ${PYTHON} >> ${BUGREPORT}
 ${PYTHON} --version >> ${BUGREPORT} 2>&1
 
+echo "---" >> ${BUGREPORT}
+
 echo Getting info for ${PYTHON_PACKAGES}
 IFS=' '
 read -ra PYTHON_PACKAGES_ARRAY <<< "$PYTHON_PACKAGES"
 for package in "${PYTHON_PACKAGES_ARRAY[@]}"; do \
-    echo getting info for ${package}
+    echo getting info for ${package}; \
     pip show ${package} >> ${BUGREPORT}; \
+    echo "---" >> ${BUGREPORT}; \
 done
 
 if [ -f garmin.py ]; then
