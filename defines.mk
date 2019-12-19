988137075e27510cc6d13f42204d4e32b9362d4b
Tom Goetz <tcgoetz@gmail.com>
2019-12-19 17:08:21 -0500

    Issue #40: fail if python can't be found

--- defines.mk
+++ defines.mk
@@ -1,13 +1,20 @@
 
 # DEFAULT_PYTHON=yes
-DEFAULT_PYTHON=no
+DEFAULT_PYTHON ?= yes
 PLATFORM=$(shell uname)
 
 #
-# Handle multiple Pythonm installs. What python are we using?
+# Handle multiple Python installs. What python are we using?
 #
 
-ifeq ($(PLATFORM), Darwin) # MacOS
+ifeq ($(PLATFORM), Linux)
+
+PYTHON2=$(shell which python)
+PIP3=$(shell which pip3)
+PYTHON3=$(shell which python3)
+
+else ifeq ($(PLATFORM), Darwin) # MacOS
+
 ifeq ($(DEFAULT_PYTHON), yes)
 PYTHON2=$(shell which python)
 PIP3=$(shell which pip3)
@@ -19,14 +26,26 @@ PYTHON3=/usr/local/bin/python3
 PIP3=/usr/local/bin/pip3
 endif
 
-else ifeq ($(PLATFORM), Linux)
+else
+
 PYTHON2=$(shell which python)
 PIP3=$(shell which pip3)
 PYTHON3=$(shell which python3)
+
 endif
 
 
 #PYTHON=${PYTHON2}
 PYTHON=$(PYTHON3)
 PIP=$(PIP3)
+
+
+ifeq ($(PYTHON),)
+$(error Python not found)
+endif
+ifeq ($(PIP),)
+$(error pip not found)
+endif
+
+
 export PYTHON PIP
