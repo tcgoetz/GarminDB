5fdc418981565430873a83f8ffcfa2340e2825bc
Tom Goetz <tcgoetz@gmail.com>
2019-12-29 09:42:56 -0500

    Issue #41: add missed new file

---
+++ version_info.py
@@ -0,0 +1,15 @@
+"""Version information for the application."""
+
+__author__ = "Tom Goetz"
+__copyright__ = "Copyright Tom Goetz"
+__license__ = "GPL"
+
+
+python_required = (3, 0, 0)
+python_tested = (3, 7, 5)
+version_info = (1, 1, 1)
+
+
+def version_string():
+    """Return a version string for a version tuple."""
+    return '.'.join(str(digit) for digit in version_info)
