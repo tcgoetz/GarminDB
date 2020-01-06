7ecf4245c3a6bc90b0e6ca505a81546d0d83165d
Tom Goetz <tcgoetz@gmail.com>
2020-01-06 08:06:45 -0500

    add new winter sports, remap wintersports to matching original versions

--- garmin_connect_enums.py
+++ garmin_connect_enums.py
@@ -39,112 +39,143 @@ class Event(enum.Enum):
 class Sport(enum.Enum):
     """Garmin Connect sport types enum."""
 
-    running                         = 1
-    cycling                         = 2
-    hiking                          = 3
-    other                           = 4
-    mountain_biking                 = 5
-    trail_running                   = 6
-    street_running                  = 7
-    track_running                   = 8
-    walking                         = 9
-    road_biking                     = 10
-    indoor_cardio                   = 11
-    strength_training               = 13
-    casual_walking                  = 15
-    speed_walking                   = 16
-    top_level                       = 17
-    treadmill_running               = 18
-    cyclocross                      = 19
-    downhill_biking                 = 20
-    track_cycling                   = 21
-    recumbent_cycling               = 22
-    indoor_cycling                  = 25
-    swimming                        = 26
-    lap_swimming                    = 27
-    open_water_swimming             = 28
-    fitness_equipment               = 29
-    elliptical                      = 30
-    stair_climbing                  = 31
-    indoor_rowing                   = 32
-    snow_shoe                       = 36
-    mountaineering                  = 37
-    rowing                          = 39
-    wind_kite_surfing               = 41
-    horseback_riding                = 44
-    driving_general                 = 49
-    flying                          = 52
-    paddling                        = 57
-    whitewater_rafting_kayaking     = 60
-    skating                         = 62
-    inline_skating                  = 63
-    resort_skiing_snowboarding      = 67
-    backcountry_skiing_snowboarding = 68
-    boating                         = 75
-    sailing                         = 77
-    cross_country_skiing            = 81
-    stand_up_paddleboarding         = 87
-    golf                            = 88
-    bmx                             = 131
-    hunting_fishing                 = 133
-    surfing                         = 137
-    wakeboarding                    = 138
-    rock_climbing                   = 139
-    hang_gliding                    = 140
-    tennis                          = 142
-    gravel_cycling                  = 143
-    diving                          = 144
-    yoga                            = 149
-    floor_climbing                  = 150
-    virtual_ride                    = 152
-    virtual_run                     = 153
-    obstacle_run                    = 154
-    indoor_running                  = 156
-    safety                          = 157
-    assistance                      = 158
-    incident_detected               = 159
-    ccr_diving                      = 161
-    auto_racing                     = 162
-    yoga_gym                        = 163
-    winter_sports                   = 165
-    snow_shoe_ws                    = 167
+    running                                 = 1
+    cycling                                 = 2
+    hiking                                  = 3
+    other                                   = 4
+    mountain_biking                         = 5
+    trail_running                           = 6
+    street_running                          = 7
+    track_running                           = 8
+    walking                                 = 9
+    road_biking                             = 10
+    indoor_cardio                           = 11
+    strength_training                       = 13
+    casual_walking                          = 15
+    speed_walking                           = 16
+    top_level                               = 17
+    treadmill_running                       = 18
+    cyclocross                              = 19
+    downhill_biking                         = 20
+    track_cycling                           = 21
+    recumbent_cycling                       = 22
+    indoor_cycling                          = 25
+    swimming                                = 26
+    lap_swimming                            = 27
+    open_water_swimming                     = 28
+    fitness_equipment                       = 29
+    elliptical                              = 30
+    stair_climbing                          = 31
+    indoor_rowing                           = 32
+    snow_shoe                               = 36
+    mountaineering                          = 37
+    rowing                                  = 39
+    wind_kite_surfing                       = 41
+    horseback_riding                        = 44
+    driving_general                         = 49
+    flying                                  = 52
+    paddling                                = 57
+    whitewater_rafting_kayaking             = 60
+    skating                                 = 62
+    inline_skating                          = 63
+    resort_skiing_snowboarding              = 67
+    backcountry_skiing_snowboarding         = 68
+    boating                                 = 75
+    sailing                                 = 77
+    cross_country_skiing                    = 81
+    stand_up_paddleboarding                 = 87
+    golf                                    = 88
+    bmx                                     = 131
+    hunting_fishing                         = 133
+    surfing                                 = 137
+    wakeboarding                            = 138
+    rock_climbing                           = 139
+    hang_gliding                            = 140
+    tennis                                  = 142
+    gravel_cycling                          = 143
+    diving                                  = 144
+    yoga                                    = 149
+    floor_climbing                          = 150
+    virtual_ride                            = 152
+    virtual_run                             = 153
+    obstacle_run                            = 154
+    indoor_running                          = 156
+    safety                                  = 157
+    assistance                              = 158
+    incident_detected                       = 159
+    ccr_diving                              = 161
+    auto_racing                             = 162
+    yoga_gym                                = 163
+    breathwork                              = 164
+    winter_sports                           = 165
+    snow_shoe_ws                            = 167
+    skating_ws                              = 168
+    backcountry_skiing_snowboarding_ws      = 169
+    skate_skiing_ws                         = 170
+    cross_country_skiing_ws                 = 171
+    resort_skiing_snowboarding_ws           = 172
+
+    @classmethod
+    def __activity_from_json(cls, json_data):
+        return json_data['activityType']
+
+    @classmethod
+    def __activity_from_details_json(cls, json_data):
+        return json_data['activityTypeDTO']
+
+    @classmethod
+    def __sport_from_json(cls, json_activity):
+        return json_activity['parentTypeId']
+
+    @classmethod
+    def __subsport_from_json(cls, json_activity):
+        return json_activity['typeId']
+
+    @classmethod
+    def __get_sport(cls, json_activity_id):
+        remaps = {
+            167 : 36,
+            168 : 62,
+            169 : 68,
+            171 : 81,
+            172 : 67
+        }
+        if json_activity_id in remaps.keys():
+            json_activity_id = remaps[json_activity_id]
+        return Sport(json_activity_id)
 
     @classmethod
     def from_json(cls, json_data):
         """Create a Sport enum instance from Garmin Connect JSON data."""
-        json_activity = json_data['activityType']
+        json_activity = cls.__activity_from_json(json_data)
         try:
-            return Sport(json_activity['parentTypeId'])
+            return cls.__get_sport(cls.__sport_from_json(json_activity))
         except ValueError:
             logger.error("Unknown sport type: %r", json_activity)
-            raise
 
     @classmethod
     def from_details_json(cls, json_data):
         """Create a Sport enum instance from Garmin Connect JSON details data."""
-        json_activity = json_data['activityTypeDTO']
+        json_activity = cls.__activity_from_details_json(json_data)
         try:
-            return Sport(json_activity['parentTypeId'])
+            return cls.__get_sport(cls.__sport_from_json(json_activity))
         except ValueError:
             logger.error("Unknown sport type: %r", json_activity)
-            raise
 
     @classmethod
     def subsport_from_json(cls, json_data):
         """Create a Sport enum instance from Garmin Connect subsport JSON data."""
-        json_activity = json_data['activityType']
+        json_activity = cls.__activity_from_json(json_data)
         try:
-            return Sport(json_activity['typeId'])
+            return cls.__get_sport(cls.__subsport_from_json(json_activity))
         except ValueError:
             logger.error("Unknown subsport type: %r", json_activity)
-            raise
 
     @classmethod
     def subsport_from_details_json(cls, json_data):
         """Create a Sport enum instance from Garmin Connect subsport JSON details data."""
-        json_activity = json_data['activityTypeDTO']
+        json_activity = cls.__activity_from_details_json(json_data)
         try:
-            return Sport(json_activity['typeId'])
+            return cls.__get_sport(cls.__subsport_from_json(json_activity))
         except ValueError:
             logger.error("Unknown subsport type: %r", json_activity)
-            raise
