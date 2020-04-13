128aa16ae0c063e273442db8f21c658ceff154fe
Tom Goetz <tomcgoetz@icloud.com>
2020-04-13 09:06:35 -0400

    test for db get methods, key-value get_type methods, mesurement_typew get; formatting

--- test/test_garmin_db_objects.py
+++ test/test_garmin_db_objects.py
@@ -14,7 +14,7 @@ import garmin_db_config_manager as GarminDBConfigManager
 
 
 root_logger = logging.getLogger()
-handler = logging.FileHandler('garmin_b_objects.log', 'w')
+handler = logging.FileHandler('garmin_db_objects.log', 'w')
 root_logger.addHandler(handler)
 root_logger.setLevel(logging.INFO)
 
@@ -27,9 +27,9 @@ class TestGarminDbObjects(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.db_params = GarminDBConfigManager.get_db_params(test_db=True)
+        cls.garmin_db = GarminDB.GarminDB(cls.db_params)
 
     def check_file_obj(self, filename_with_path, file_type, file_serial_number):
-        garmindb = GarminDB.GarminDB(self.db_params)
         (file_id, file_name) = GarminDB.File.name_and_id_from_path(filename_with_path)
         file_dict = {
             'id'            : file_id,
@@ -38,8 +38,8 @@ class TestGarminDbObjects(unittest.TestCase):
             'serial_number' : file_serial_number,
         }
         logger.info("check_file_table: %r", file_dict)
-        GarminDB.File.insert_or_update(garmindb, file_dict)
-        file = GarminDB.File.get(garmindb, file_id)
+        GarminDB.File.insert_or_update(self.garmin_db, file_dict)
+        file = GarminDB.File.get(self.garmin_db, file_id)
         self.assertEqual(file.id, file_dict['id'])
         self.assertEqual(file.name, file_dict['name'])
         self.assertEqual(file.type, file_dict['type'])
@@ -65,6 +65,46 @@ class TestGarminDbObjects(unittest.TestCase):
     def test_file_type(self):
         file_types_list = list(GarminDB.File.FileType)
         self.assertIn(GarminDB.File.FileType.convert(Fit.FileType.goals), file_types_list)
+
+    def test_get_default(self):
+        result = GarminDB.Attributes.get(self.garmin_db, 'test_String')
+        self.assertEqual(result, None)
+        result = GarminDB.Attributes.get(self.garmin_db, 'test_String', 'default_value')
+        self.assertEqual(result, 'default_value')
+        #
+        result = GarminDB.Attributes.get_string(self.garmin_db, 'test_String')
+        self.assertEqual(result, None)
+        result = GarminDB.Attributes.get_string(self.garmin_db, 'test_String', 'default_value')
+        self.assertEqual(result, 'default_value')
+
+    def test_set_get_default(self):
+        GarminDB.Attributes.set(self.garmin_db, 'test_String', 'test_value')
+        result = GarminDB.Attributes.get_string(self.garmin_db, 'test_String')
+        self.assertEqual(result, 'test_value')
+        result = GarminDB.Attributes.get_string(self.garmin_db, 'test_String', 'default_value')
+        self.assertEqual(result, 'test_value')
+        #
+        GarminDB.Attributes.set_if_unset(self.garmin_db, 'test_String', 'test_value2')
+        result = GarminDB.Attributes.get_string(self.garmin_db, 'test_String')
+        self.assertEqual(result, 'test_value')
+
+    def test_get_float(self):
+        GarminDB.Attributes.set(self.garmin_db, 'test_String', 2.2)
+        result = GarminDB.Attributes.get_float(self.garmin_db, 'test_String')
+        self.assertEqual(result, 2.2)
+        result = GarminDB.Attributes.get_float(self.garmin_db, 'test_String', 'default_value')
+        self.assertEqual(result, 2.2)
+        #
+        GarminDB.Attributes.set_if_unset(self.garmin_db, 'test_String', 2.2)
+        result = GarminDB.Attributes.get_float(self.garmin_db, 2.2)
+
+    def test_measurement_system(self):
+        result = GarminDB.Attributes.measurements_type(self.garmin_db, Fit.field_enums.DisplayMeasure.metric)
+        self.assertEqual(result, Fit.field_enums.DisplayMeasure.metric)
+        for value in Fit.field_enums.DisplayMeasure:
+            GarminDB.Attributes.set(self.garmin_db, 'measurement_system', value)
+            result = GarminDB.Attributes.measurements_type(self.garmin_db)
+            self.assertEqual(result, value)
 
 
 if __name__ == '__main__':
