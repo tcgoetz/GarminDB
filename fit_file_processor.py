ee270df43e4611d2b32196f206808fe185090b8c
Tom Goetz <tomcgoetz@icloud.com>
2021-05-24 09:11:30 -0400

    fix merge from develop

--- fit_file_processor.py
+++ fit_file_processor.py
@@ -81,11 +81,9 @@ class FitFileProcessor(object):
         priority_message_types = [Fit.MessageType.file_id, Fit.MessageType.device_info]
         for message_type in priority_message_types:
             self.__write_message_type(fit_file, message_type)
-            self.garmin_db_session.commit()
         for message_type in message_types:
             if message_type not in priority_message_types:
                 self.__write_message_type(fit_file, message_type)
-                self.garmin_db_session.commit()
 
     def write_file(self, fit_file):
         """Write all data from the FIT file to database files."""
