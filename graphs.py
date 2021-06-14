356eed92e01e9acde22ad76a858308bb1a8b3b4a
Tom Goetz <tomcgoetz@icloud.com>
2021-06-14 07:56:11 -0400

    first pass at analysis using Jupyter notebook

--- graphs.py
+++ graphs.py
@@ -71,7 +71,7 @@ class Graph(object):
         'months'    : HealthDB.MonthsSummary
     }
 
-    def __init__(self, debug, save):
+    def __init__(self, debug=False, save=False):
         """Return an instance of the Graph class."""
         self.debug = debug
         self.save = save
@@ -87,12 +87,12 @@ class Graph(object):
         return data
 
     @classmethod
-    def __graph_multiple_single_axes(cls, time, data_list, stat_name, ylabel, save):
+    def __graph_multiple_single_axes(cls, time, data_list, stat_name, ylabel, save, geometry=111):
         title = f'{stat_name} Over Time'
         figure = plt.figure(figsize=GarminDBConfigManager.get_graphs('size'))
         for index, data in enumerate(data_list):
             color = Colors.from_integer(index).name
-            axes = figure.add_subplot(111, frame_on=(index == 0))
+            axes = figure.add_subplot(geometry, frame_on=(index == 0))
             axes.plot(time, data, color=color)
             axes.grid()
         axes.set_title(title)
@@ -100,15 +100,14 @@ class Graph(object):
         axes.set_ylabel(ylabel)
         if save:
             figure.savefig(stat_name + ".png")
-        plt.show()
 
     @classmethod
-    def __graph_multiple(cls, time, data_list, stat_name, period, ylabel_list, yrange_list, save):
+    def __graph_multiple(cls, time, data_list, stat_name, period, ylabel_list, yrange_list, save, geometry=111):
         title = f'{stat_name} by {period}'
         figure = plt.figure(figsize=GarminDBConfigManager.get_graphs('size'))
         for index, data in enumerate(data_list):
             color = Colors.from_integer(index).name
-            axes = figure.add_subplot(111, label=ylabel_list[index], frame_on=(index == 0))
+            axes = figure.add_subplot(geometry, label=ylabel_list[index], frame_on=(index == 0))
             axes.plot(time, data, color=color)
             axes.set_ylabel(ylabel_list[index], color=color)
             axes.yaxis.set_label_position(YAxisLabelPostion.from_integer(index).name)
@@ -126,13 +125,12 @@ class Graph(object):
         axes.set_xlabel('Time')
         if save:
             figure.savefig(stat_name + ".png")
-        plt.show()
 
     @classmethod
-    def __graph_over(cls, date, over_data_dicts, under_data_dict, title, xlabel, ylabel, save_name=None):
+    def __graph_over(cls, date, over_data_dicts, under_data_dict, title, xlabel, ylabel, save_name=None, geometry=111):
         figure = plt.figure(figsize=GarminDBConfigManager.get_graphs('size'))
         # First graph the data that appears under
-        axes = figure.add_subplot(111, frame_on=True)
+        axes = figure.add_subplot(geometry, frame_on=True)
         axes.fill_between(under_data_dict['time'], under_data_dict['data'], 0, color=Colors.c.name)
         axes.set_ylim(under_data_dict['limits'])
         axes.set_xticks([])
@@ -143,7 +141,7 @@ class Graph(object):
             over_data_dict = over_data_dicts[index]
             color = colors[index]
             label = over_data_dict['label']
-            axes = figure.add_subplot(111, frame_on=False, label=label)
+            axes = figure.add_subplot(geometry, frame_on=False, label=label)
             axes.plot(over_data_dict['time'], over_data_dict['data'], color=color)
             axes.set_ylabel(label, color=color)
             axes.yaxis.set_label_position(YAxisLabelPostion.from_integer(index).name)
@@ -162,32 +160,31 @@ class Graph(object):
         axes.xaxis.set_major_formatter(x_format)
         if save_name:
             figure.savefig(save_name)
-        plt.show()
 
-    def _graph_steps(self, time, data, period):
+    def _graph_steps(self, time, data, period, geometry=111):
         steps = self.__remove_discontinuities([entry.steps for entry in data])
         steps_goal_percent = self.__remove_discontinuities([entry.steps_goal_percent for entry in data])
         yrange_list = [(0, max(steps) * 1.1), (0, max(steps_goal_percent) * 2)]
-        self.__graph_multiple(time, [steps, steps_goal_percent], 'Steps', period, ['Steps', 'Step Goal Percent'], yrange_list, self.save)
+        self.__graph_multiple(time, [steps, steps_goal_percent], 'Steps', period, ['Steps', 'Step Goal Percent'], yrange_list, self.save, geometry)
 
-    def _graph_hr(self, time, data, period):
+    def _graph_hr(self, time, data, period, geometry=111):
         rhr = self.__remove_discontinuities([entry.rhr_avg for entry in data])
         inactive_hr = self.__remove_discontinuities([entry.inactive_hr_avg for entry in data])
-        self.__graph_multiple(time, [rhr, inactive_hr], 'Heart Rate', period, ['RHR', 'Inactive hr'], [(30, 100), (30, 100)], self.save)
+        self.__graph_multiple(time, [rhr, inactive_hr], 'Heart Rate', period, ['RHR', 'Inactive hr'], [(30, 100), (30, 100)], self.save, geometry)
 
-    def _graph_itime(self, time, data, period):
+    def _graph_itime(self, time, data, period, geometry=111):
         itime = [entry.intensity_time_mins for entry in data]
         itime_goal_percent = self.__remove_discontinuities([entry.intensity_time_goal_percent for entry in data])
         itime_goal_max = max([entry.intensity_time_goal_mins for entry in data])
         yrange_list = [(0, itime_goal_max * 5), (0, max(itime_goal_percent) * 1.1)]
         self.__graph_multiple(time, [itime, itime_goal_percent], 'Intensity Minutes', period, ['Intensity Minutes', 'Intensity Minutes Goal Percent'],
-                              yrange_list, self.save)
+                              yrange_list, self.save, geometry)
 
-    def _graph_weight(self, time, data, period):
+    def _graph_weight(self, time, data, period, geometry=111):
         weight = [entry.weight_avg for entry in data]
-        self.__graph_multiple_single_axes(time, [weight], 'Weight', 'weight', self.save)
+        self.__graph_multiple_single_axes(time, [weight], 'Weight', 'weight', self.save, geometry)
 
-    def graph_activity(self, activity, period, days):
+    def graph_activity(self, activity, period=None, days=None, geometry=111):
         """Generate a graph for the given activity with points every period spanning days."""
         if period is None:
             period = GarminDBConfigManager.graphs_activity_config(activity, 'period')
@@ -205,7 +202,7 @@ class Graph(object):
             time = [entry.first_day for entry in data]
         graph_func_name = '_graph_' + activity
         graph_func = getattr(self, graph_func_name, None)
-        graph_func(time, data, period)
+        graph_func(time, data, period, geometry)
 
     def __format_steps(self, data):
         steps = []
@@ -220,7 +217,7 @@ class Graph(object):
             steps.append(sum(steps_count.values()))
         return steps
 
-    def graph_date(self, date):
+    def graph_date(self, date, geometry=111):
         """Generate a graph for the given date."""
         if date is None:
             date = (datetime.datetime.now() - datetime.timedelta(days=1)).date()
@@ -251,7 +248,7 @@ class Graph(object):
         # self.__graph_day(date, (hr_time, hr), (mon_time, activity), self.save)
         save_name = f"{date}_daily.png" if self.save else None
         self.__graph_over(date, over_data_dict, under_data_dict, f'Daily Summary for {date}: Heart Rate and Steps over Activity',
-                          'Time of Day', 'heart rate', save_name=save_name)
+                          'Time of Day', 'heart rate', save_name=save_name, geometry=geometry)
 
 
 def main(argv):
@@ -286,18 +283,23 @@ def main(argv):
 
     if Statistics.rhr in args.stats:
         graph.graph_activity('hr', args.period, args.days)
+        plt.show()
 
     if Statistics.itime in args.stats:
         graph.graph_activity('itime', args.period, args.days)
+        plt.show()
 
     if Statistics.steps in args.stats:
         graph.graph_activity('steps', args.period, args.days)
+        plt.show()
 
     if Statistics.weight in args.stats:
         graph.graph_activity('weight', args.period, args.days)
+        plt.show()
 
     if args.day:
         graph.graph_date(args.day)
+        plt.show()
 
 
 if __name__ == "__main__":
