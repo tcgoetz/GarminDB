cf7220e90e49476e769b496154198194d3e42e00
Tom Goetz <tomcgoetz@icloud.com>
2022-01-10 17:45:21 -0500

    markdown based notebooks

--- garmindb/analyze.py
+++ garmindb/analyze.py
@@ -11,12 +11,11 @@ import calendar
 from tqdm import tqdm
 
 import fitfile
-from idbutils.list_and_dict import list_not_none
 
 from garmindb import summarydb
 from .garmindb import GarminDb, Attributes, File, Weight, Stress, RestingHeartRate, IntensityHR, Sleep
 from .garmindb import MonitoringDb, Monitoring, MonitoringHeartRate, MonitoringIntensity, MonitoringClimb
-from .garmindb import ActivitiesDb, Activities, ActivityRecords, ActivityLaps, StepsActivities
+from .garmindb import ActivitiesDb, Activities, StepsActivities
 from .garmindb import GarminSummaryDb, Summary, DaysSummary, DailySummary, WeeksSummary, MonthsSummary, YearsSummary
 from .garmin_connect_config_manager import GarminConnectConfigManager
 
@@ -57,126 +56,9 @@ class Analyze(object):
         for file_type_name in [file_type.name for file_type in File.FileType]:
             self.__report_file_type(file_type_name)
 
-    def __report_sport(self, sport_col, sport):
-        fitfile.units.unit_strings[self.measurement_system]
-        records = Activities.row_count(self.garmin_act_db, sport_col, sport)
-        if records > 0:
-            sport_title = sport.title().replace('_', ' ')
-            total_distance = Activities.get_col_sum_for_value(self.garmin_act_db, Activities.distance, sport_col, sport)
-            if total_distance is None:
-                total_distance = 0
-                average_distance = 0
-            else:
-                average_distance = total_distance / records
-            distance_units = self.unit_strings[fitfile.units.UnitTypes.distance_long]
-            stat_logger.info("%s activities: %d - total distance %d %s average distance %d %s",
-                             sport_title, records, total_distance, distance_units, average_distance, distance_units)
-            self.__save_summary_stat(sport + '_Activities', records)
-            self.__save_summary_stat(sport + '_Miles', total_distance)
-
-    def __get_activities_stats(self):
-        stat_logger.info("___Activities Statistics___")
-        activities = Activities.row_count(self.garmin_act_db)
-        stat_logger.info("Total activities: %d", activities)
-        self.__save_summary_stat('Activities', activities)
-        laps = ActivityLaps.row_count(self.garmin_act_db)
-        stat_logger.info("Activities lap records: %d", laps)
-        self.__save_summary_stat('Activity_laps', laps)
-        records = ActivityRecords.row_count(self.garmin_act_db)
-        stat_logger.info("Activity records: %d", records)
-        self.__save_summary_stat('Activity_records', records)
-        years = Activities.get_years(self.garmin_act_db)
-        stat_logger.info("Years with activities: %d: %s", len(years), years)
-        self.__save_summary_stat('Activity_Years', len(years))
-        fitness_activities = Activities.row_count(self.garmin_act_db, Activities.type, 'fitness')
-        stat_logger.info("Fitness activities: %d", fitness_activities)
-        self.__save_summary_stat('Fitness_activities', fitness_activities)
-        recreation_activities = Activities.row_count(self.garmin_act_db, Activities.type, 'recreation')
-        stat_logger.info("Recreation activities: %d", recreation_activities)
-        self.__save_summary_stat('Recreation_activities', recreation_activities)
-        sports = list_not_none(Activities.get_col_distinct(self.garmin_act_db, Activities.sport))
-        stat_logger.info("Sports: %s", ', '.join(sports))
-        sub_sports = list_not_none(Activities.get_col_distinct(self.garmin_act_db, Activities.sub_sport))
-        stat_logger.info("SubSports: %s", ', '.join(sub_sports))
-        for sport_name in [sport.name for sport in fitfile.Sport]:
-            self.__report_sport(Activities.sport, sport_name)
-
-    def __get_col_stats(self, table, col, name, ignore_le_zero=False, time_col=False):
-        records = table.row_count(self.garmin_db)
-        self.__save_summary_stat('%s_Records' % name, records)
-        if time_col:
-            maximum = table.get_time_col_max(self.garmin_db, col)
-        else:
-            maximum = table.get_col_max(self.garmin_db, col)
-        self.__save_summary_stat('Max_%s' % name, maximum)
-        if time_col:
-            minimum = table.get_time_col_min(self.garmin_db, col)
-        else:
-            minimum = table.get_col_min(self.garmin_db, col, None, None, ignore_le_zero)
-        self.__save_summary_stat('Min_%s' % name, minimum)
-        if time_col:
-            average = table.get_time_col_avg(self.garmin_db, col)
-        else:
-            average = table.get_col_avg(self.garmin_db, col, None, None, ignore_le_zero)
-        self.__save_summary_stat('Avg_%s' % name, average)
-        latest = table.get_col_latest(self.garmin_db, col)
-        stat_logger.info("%s records: %s max: %s min: %s avg: %s latest: %s", name, records, maximum, minimum, average, latest)
-
-    def __get_monitoring_stats(self):
-        stat_logger.info("___Monitoring Statistics___")
-        self.__get_col_stats(Weight, Weight.weight, 'Weight')
-        self.__get_col_stats(Stress, Stress.stress, 'Stress', True)
-        self.__get_col_stats(RestingHeartRate, RestingHeartRate.resting_heart_rate, 'RHR', True)
-        self.__get_col_stats(Sleep, Sleep.total_sleep, 'Sleep', True, True)
-        self.__get_col_stats(Sleep, Sleep.rem_sleep, 'REM Sleep', True, True)
-
-    def __get_monitoring_days(self, year):
-        days = Monitoring.get_days(self.garmin_mon_db, year)
-        days_count = len(days)
-        if days_count > 0:
-            first_day = days[0]
-            last_day = days[-1]
-            span = last_day - first_day + 1
-        else:
-            span = 0
-        self.__save_summary_stat(str(year) + '_days', days_count)
-        self.__save_summary_stat(str(year) + '_days_span', span)
-        stat_logger.info("%d Days with data (%d count vs %d span): %s", year, days_count, span, days)
-        for index in range(days_count - 1):
-            day = int(days[index])
-            next_day = int(days[index + 1])
-            if next_day != day + 1:
-                day_str = str(fitfile.conversions.day_of_the_year_to_datetime(year, day))
-                next_day_str = str(fitfile.conversions.day_of_the_year_to_datetime(year, next_day))
-                stat_logger.info("Days gap between %d (%s) and %d (%s)", day, day_str, next_day, next_day_str)
-        return days_count
-
-    def __get_monitoring_months(self, year):
-        months = Monitoring.get_month_names(self.garmin_mon_db, year)
-        self.__save_summary_stat(str(year) + '_months', len(months))
-        stat_logger.info("%s Months with data (%s): %s", year, len(months), months)
-
-    def __get_monitoring_years(self):
-        stat_logger.info("___Monitoring Records Coverage___")
-        stat_logger.info("This shows periods that data has been downloaded for. "
-                         "Not seeing data for days you know Garmin has data? "
-                         "Change the starting day and the number of days in GarminConnectConfig.json and do a full download.")
-        years = Monitoring.get_years(self.garmin_mon_db)
-        self.__save_summary_stat('Monitoring_Years', len(years))
-        stat_logger.info("Monitoring records: %d", Monitoring.row_count(self.garmin_mon_db))
-        stat_logger.info("Monitoring Years with data (%d): %s", len(years), years)
-        total_days = 0
-        for year in years:
-            self.__get_monitoring_months(year)
-            total_days += self.__get_monitoring_days(year)
-        stat_logger.info("Total days with monitoring data: %d", total_days)
-
     def get_stats(self):
         """Calculate summary statistics."""
         self.__get_files_stats()
-        self.__get_activities_stats()
-        self.__get_monitoring_stats()
-        self.__get_monitoring_years()
 
     def __populate_hr_intensity(self, day_date, garmin_mon_session, garmin_sum_session, overwrite=False):
         if IntensityHR.s_row_count_for_day(garmin_sum_session, day_date) == 0 or overwrite:
