6b1dbfd838be2f363dce8bdcca2dc328ab36bd42
Tom Goetz <tomcgoetz@icloud.com>
2022-10-21 08:12:01 -0400

    3.2.6

--- garmindb/version_info.py
+++ garmindb/version_info.py
@@ -7,8 +7,8 @@ __license__ = "GPL"
 
 python_required = (3, 0, 0)
 python_tested = (3, 9, 10)
-version_info = (3, 2, 5)
-prerelease = False
+version_info = (3, 2, 6)
+prerelease = True
 
 
 def version_string():
