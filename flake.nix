1c05603ced12672a95b9a1704ec1e35d4ec488d0
Federico Izzo <federico.izzo99@gmail.com>
2022-12-22 12:00:04 +0100

    add home manager module

--- flake.nix
+++ flake.nix
@@ -2,51 +2,118 @@
   description = "Download and parse data from Garmin Connect or a Garmin watch, FitBit CSV, and MS Health CSV files into and analyze data in Sqlite serverless databases with Jupyter notebooks.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-
+    flake-parts.url = "github:hercules-ci/flake-parts";
     pypi-deps-db = {
       url = "github:DavHau/pypi-deps-db";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mach-nix = {
-      url = "github:DavHau/mach-nix/3.5.0";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.pypi-deps-db.follows = "pypi-deps-db";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@attr:
+  outputs = inputs@{ self, flake-parts, ... }:
     let
       requirements = (builtins.readFile ./requirements.txt);
       devRequirements = (builtins.readFile ./dev-requirements.txt);
-      # cleanedRequirements = (builtins.replaceStrings [ ] [ ] requirements);
+      cleanedRequirements = (builtins.replaceStrings [
+        ">=1.4.0"
+        ">=1.1.3"
+        ">=1.0.4"
+        ">=1.0.6"
+        "ipyleaflet"
+      ] [ "" "" "" "" "" ]
+        requirements);
+      splitRequirements = (builtins.filter (el: builtins.isString el && el != "") (builtins.split "\n" cleanedRequirements));
     in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        mach = mach-nix.lib.${system};
-        garminDBDev = mach.mkPython {
-          python = "python39";
-          requirements = devRequirements;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem = { pkgs, lib, config, system, inputs', ... }:
+        let
+          fitfile = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "fitfile";
+            version = "1.1.3";
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-KfupwZ8KsaxCLEB6e7Zve8HJA9ewmDAKSulNwPaSouE=";
+            };
+          };
+          idbutils = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "idbutils";
+            version = "1.0.6";
+            buildInputs = with pkgs.python3Packages; [ python-dateutil requests sqlalchemy tqdm ];
+            propagateBuildInputs = with pkgs.python3Packages; [ python-dateutil requests sqlalchemy tqdm ];
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-dU5YfJ7ae6v186e9uII/68AkLIsUnd7UvGCB2nKDp6E=";
+            };
+          };
+          tcxfile = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "tcxfile";
+            version = "1.0.4";
+            buildInputs = with pkgs.python3Packages; [ python-dateutil ];
+            propagateBuildInputs = with pkgs.python3Packages; [ python-dateutil ];
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-qJL25YZd32DosOm6RT9nzh0DaXBWpr9muGpJSws6p5M=";
+            };
+          };
+          xyzservices = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "xyzservices";
+            version = "2022.9.0";
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-VWUZYXCLmhSEmXizOd92AIyIbfeoMmMIpVSbrlUWJgw=";
+            };
+            doCheck = false;
+          };
+          ipyleaflet = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "ipyleaflet";
+            version = "0.17.2";
+            buildInputs = with pkgs.python3Packages; [
+              jupyter-packaging
+              traittypes
+              ipywidgets
+              branca
+            ] ++ [ xyzservices ];
+            propagateBuildInputs = with pkgs.python3Packages; [
+              jupyter-packaging
+              traittypes
+              ipywidgets
+              branca
+            ] ++ [ xyzservices ];
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-gRzrShVYmqA0qAckz+W9yTaJVjuIn0ub7T1eO8t2Jrs=";
+            };
+            doCheck = false;
+          };
+          garminDB = pkgs.python3Packages.buildPythonPackage {
+            name = "garmindb";
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              python-dateutil
+              cloudscraper
+              tqdm
+              matplotlib
+              sqlalchemy
+              ipykernel
+              cached-property
+              traittypes
+              ipywidgets
+              branca
+            ] ++ [ fitfile idbutils tcxfile ipyleaflet xyzservices ];
+            src = ./.;
+            doCheck = false;
+          };
+        in
+        {
+          packages.default = garminDB;
+          devShells.default = garminDB;
         };
-
-        garminDB = mach.mkPython {
-          python = "python39";
-
-          requirements = requirements;
-          packagesExtra = [
-            ./.
-          ];
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [
-            garminDBDev
-          ];
-        };
-        defaultPackage = garminDB;
-      }
-    );
+      flake = {
+        homeManagerModule = import ./nix/hm-module.nix self;
+      };
+    };
 }
+
+
---
+++ nix/hm-module.nix
@@ -0,0 +1,159 @@
+self: { config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.programs.garminDB;
+  stats = [ "monitoring" "steps" "itime" "sleep" "rhr" "weight" "activities" ];
+  garminDBPackage = self.packages."${pkgs.system}".default;
+  credentialsOption = {
+    options = {
+      user = mkOption {
+        type = types.str;
+        description = "Username";
+      };
+      secure_password = mkOption {
+        type = types.bool;
+        description = "Secure password";
+      };
+      password = mkOption {
+        type = types.str;
+        description = "Password";
+        default = "";
+      };
+      password_command = mkOption {
+        type = types.str;
+        description = "Command to retrieve the password";
+      };
+    };
+  };
+  dataOption = {
+    options = {
+      weight_start_date = mkOption {
+        type = types.str;
+        description = "The date to start downloading weight data from (dd/mm/YYYY)";
+      };
+      sleep_start_date = mkOption {
+        type = types.str;
+        description = "The date to start downloading sleep data from (dd/mm/YYYY)";
+      };
+      rhr_start_date = mkOption {
+        type = types.str;
+        description = "The date to start downloading resting heart rate data from (dd/mm/YYYY)";
+      };
+      monitoring_start_date = mkOption {
+        type = types.str;
+        description = "The date to start downloading daily monitoring data from (dd/mm/YYYY)";
+      };
+      download_latest_activities = mkOption {
+        type = types.int;
+        description = "The number of activities summaries to ask Garmin Connect for when looking for the latest activities. Activities that have no been previously downloaded will be downloaded";
+      };
+      download_all_activities = mkOption {
+        type = types.int;
+        description = "The number of activities to summaries to ask for when looking for all activities to download.  Activities that have no been previously downloaded will be downloaded";
+      };
+    };
+  };
+  copyOption = {
+    options = {
+      mount_dir = mkOption {
+        type = types.str;
+        description = "TODO";
+        default = "/Volumes/GARMIN";
+      };
+    };
+  };
+  enabledStatsOption = {
+    options = {
+      monitoring = mkOption {
+        type = types.bool;
+        description = "Enabled monitoring stat";
+        default = true;
+      };
+      steps = mkOption {
+        type = types.bool;
+        description = "Enabled steps stat";
+        default = true;
+      };
+      itime = mkOption {
+        type = types.bool;
+        description = "Enabled itime stat";
+        default = true;
+      };
+      sleep = mkOption {
+        type = types.bool;
+        description = "Enabled sleep stat";
+        default = true;
+      };
+      rhr = mkOption {
+        type = types.bool;
+        description = "Enabled rhr stat";
+        default = true;
+      };
+      weight = mkOption {
+        type = types.bool;
+        description = "Enabled weight stat";
+        default = true;
+      };
+      activities = mkOption {
+        type = types.bool;
+        description = "Enabled activities stat";
+        default = true;
+      };
+    };
+  };
+  courseViewsOption = {
+    options = {
+      steps = mkOption {
+        type = types.listOf types.str;
+        description = "TODO";
+        default = [ ];
+      };
+    };
+  };
+  modesOption = {
+    options = { };
+  };
+  activitiesOption = {
+    options = {
+      display = mkOption {
+        type = types.listOf types.str;
+        description = "TODO";
+        default = [ ];
+      };
+    };
+  };
+in
+{
+  options.programs.garminDB = {
+    enable = mkEnableOption "Garming Connect downloader and backuper";
+    config.credentials = mkOption {
+      type = with types; submodule credentialsOption;
+    };
+    config.data = mkOption {
+      type = with types; submodule dataOption;
+    };
+    config.copy = mkOption {
+      type = with types; submodule copyOption;
+    };
+    config.enabled_stats = mkOption {
+      type = with types; submodule enabledStatsOption;
+    };
+    config.course_views = mkOption {
+      type = with types; submodule courseViewsOption;
+    };
+    config.modes = mkOption {
+      type = with types; submodule modesOption;
+    };
+    config.activities = mkOption {
+      type = with types; submodule activitiesOption;
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages = [ garminDBPackage ];
+    home.file.".GarminDb/GarminConnectConfig.json" = {
+      text = "${builtins.toJSON cfg.config}";
+    };
+  };
+}
+
