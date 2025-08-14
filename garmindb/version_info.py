70c69c28e4c0d4c85a890c10186fb35799bdf79d
Tom Goetz <tcgoetz@gmail.com>
2025-08-14 11:21:57 -0400

    v3.6.6 prerelease

--- garmindb/version_info.py
+++ garmindb/version_info.py
@@ -8,7 +8,7 @@ __license__ = "GPL"
 python_required = (3, 10, 0)
 dev_python_required = (3, 13, 5)
 python_tested = (3, 13, 5)
-version_info = (3, 6, 5)
+version_info = (3, 6, 6)
 prerelease = True
 
 
