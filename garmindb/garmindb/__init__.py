b27b5c7ce0d42057895eddc70d3908991dcdd3c1
alpenglowtek <132421138+alpenglowtek@users.noreply.github.com>
2026-01-24 12:56:04 -0800

    Feature/hrv support (#295)
    This adds support for importing HRV data from Garmin's HRV_STATUS.fit files,
    which contain overnight HRV measurements and daily status summaries.
    
    Changes:
    - Add MonitoringHrvValue table for individual HRV RMSSD readings during sleep
    - Add MonitoringHrvStatus table for daily HRV status summaries
    - Add HRV message handlers in MonitoringFitFileProcessor
    - Include FileType.hrv_status in monitoring FIT file processing
    - Bump monitoring DB version to 7
    
    The HRV data includes:
    - Individual RMSSD readings (5-minute intervals during sleep)
    - Weekly average RMSSD
    - Last night's average RMSSD
    - Baseline low/high bounds
    - HRV status (0=unknown, 2=poor, 3=low, 4=balanced)
    - Reading count
    
    Requires corresponding changes in the Fit submodule for:
    - FileType.hrv_status (68)
    - MessageType.hrv_status_summary (370)
    - MessageType.hrv_value (371)

--- garmindb/garmindb/__init__.py
+++ garmindb/garmindb/__init__.py
@@ -8,7 +8,7 @@ __license__ = "GPL"
 
 from .garmin_db import GarminDb, Attributes, Device, DeviceInfo, File, Weight, Stress, Sleep, SleepEvents, RestingHeartRate, DailySummary
 from .monitoring_db import MonitoringDb, MonitoringInfo, MonitoringHeartRate, MonitoringIntensity, MonitoringClimb, Monitoring, \
-    MonitoringRespirationRate, MonitoringPulseOx
+    MonitoringRespirationRate, MonitoringPulseOx, MonitoringHrvValue, MonitoringHrvStatus
 from .activities_db import ActivitiesDb, Activities, ActivityLaps, ActivityRecords, ActivitiesDevices, ActivitySplits, SportActivities, StepsActivities, \
     PaddleActivities, CycleActivities, ClimbingActivities
 from .garmin_summary_db import GarminSummaryDb, Summary, YearsSummary, MonthsSummary, WeeksSummary, DaysSummary, IntensityHR
